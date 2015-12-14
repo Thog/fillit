b1526f49a86a35d451f0a7855d208286a6dd0928
Bilal EL BAZ <bel-baz@e3r4p14.42.fr>
2015-12-14 16:21:56 +0100

    add security check + delete useless struct

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 17:19:06 by tguillem          #+#    #+#             */
-/*   Updated: 2015/12/08 12:00:01 by tguillem         ###   ########.fr       */
+/*   Updated: 2015/12/14 16:21:34 by bel-baz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,4 @@ char				*read_file(char *file);
 char				**prepare_fill(char *fname);
 char				*fillit(char **shapes);
 int					ft_strlen(const char *s);
-
-typedef struct		s_pieces
-{
-	char			*data;
-	int				x;
-	int				y;
-	int				width;
-	int				height;
-}					t_pieces;
 #endif
